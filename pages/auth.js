cfab43275daf5bd3e643a6cc6c026fff932c85f3
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-01-04 11:17:54 +0530

    first commit

---
+++ pages/auth.js
@@ -0,0 +1,26 @@
+import AuthForm from "@/components/auth/auth-form";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p style={{ textAlign: "center" }}>Loading...</p>;
+  }
+  return <AuthForm />;
+}
+
+export default AuthPage;
